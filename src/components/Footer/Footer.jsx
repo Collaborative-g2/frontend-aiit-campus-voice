d182a65fd5cd8766b1063ae96fd6b34d21dd6ee7
Daiki <z24075du@aiit.ac.jp>
2024-12-26 23:26:52 +0900

    feat: create responsive footer layout

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,11 @@
 const Footer = () => {
     return (
         <>
-            <div>Footer</div>
+            <footer className="bg-primary py-4">
+                <div className="text-center text-sm text-white">
+                    &copy; {new Date().getFullYear()} All rights reserved.
+                </div>
+            </footer>
         </>
     );
 };
