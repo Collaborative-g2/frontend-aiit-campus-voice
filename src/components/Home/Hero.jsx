59ca2f752ffdb06459bf5380341a7c571a954b0d
Daiki <z24075du@aiit.ac.jp>
2024-12-27 20:32:27 +0900

    style: fix code indentation and spacing

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,6 +1,6 @@
 import DiscussionMovie from "../../assets/discussion.mp4";
-import {AiOutlineSearch} from "react-icons/ai";
-import {useState} from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { useState } from "react";
 import axios from "axios";
 
 const ACV_API_BASE_URL = import.meta.env.VITE_ACV_API_BASE_URL;
@@ -53,24 +53,29 @@ const Hero = () => {
           muted
           className="absolute right-0 top-0 h-[700px] w-full object-cover z-[-1]"
         >
-          <source src={DiscussionMovie} type="video/mp4"/>
+          <source src={DiscussionMovie} type="video/mp4" />
         </video>
         <div className="bg-black/20 h-full">
           <div className="h-full flex justify-center items-center p-4 bg-primary/10">
             <div className="container grid grid-cols-1 gap-4 px-0">
               {/* text content section */}
               <div className="text-white text-center font-mono">
-                <p className="font-semibold text-3xl">講義の評価が一目でわかる!</p>
-                <p className="font-medium">簡単検索であなたにぴったりの授業を</p>
+                <p className="font-semibold text-3xl">
+                  講義の評価が一目でわかる!
+                </p>
+                <p className="font-medium">
+                  簡単検索であなたにぴったりの授業を
+                </p>
               </div>
               {/* Search bar section */}
               <div className="container py-4 relative w-full max-w-[1000px]">
                 <div className="relative flex items-center bg-gray-100 rounded-full">
                   {/* Search button */}
-                  <button
-                      className="absolute left-3 p-2 flex items-center jufstify-center"
-                  >
-                    <AiOutlineSearch className="w-5 h-5 text-primary" strokeWidth={30} />
+                  <button className="absolute left-3 p-2 flex items-center jufstify-center">
+                    <AiOutlineSearch
+                      className="w-5 h-5 text-primary"
+                      strokeWidth={30}
+                    />
                   </button>
                   {/* Input field */}
                   <input
@@ -98,7 +103,9 @@ const Hero = () => {
                         </li>
                       ))
                     ) : (
-                      <li className="p-2 text-gray-500">No suggestions found</li>
+                      <li className="p-2 text-gray-500">
+                        No suggestions found
+                      </li>
                     )}
                   </ul>
                 )}
