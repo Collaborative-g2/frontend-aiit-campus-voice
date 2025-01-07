c19694b6fdf848c1b5a0925d1168b297e635cdbb
Yuta Sugiyama <y.sugiyama54@gmail.com>
2025-01-07 22:54:51 +0900

    画像の差し込み

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -1,33 +1,45 @@
+import AboutLogo1 from "../../assets/about1.svg";
+import AboutLogo2 from "../../assets/about2.svg";
+import AboutLogo3 from "../../assets/about3.svg";
 
 const About = () => {
     return(
         <>
-            <div class="container mx-auto px-4 py-8 bg-orange-300">
-                <h1 class="text-center text-4xl font-bold text-gray-700 mb-8 underline">履修登録時に時こんな経験はありませんか？</h1>
-                <div class="flex flex-wrap">
-                    <div class="w-full md:w-1/3 px-4 mb-6">
-                        <div class="bg-white border border-gray-200 rounded-lg shadow p-6">
-                            <h2 class="text-lg font-semibold text-gray-700 mb-4">講義内容や講義形式がシラバスと異なる</h2>
-                            <p class="text-sm text-gray-600">講義内容や講義形式がシラバスに記載してある内容と異なり、履修してみたら自分の興味や目的と異なっていた</p>
+            <div className="container mx-auto px-4 py-8 bg-orange-300">
+                <h1 className="text-center text-4xl font-extrabold text-gray-700 mb-8 underline">履修登録時に時こんな経験はありませんか？</h1>
+                <div className="flex flex-wrap">
+                    <div className="w-full md:w-1/3 px-4 mb-6">
+                        <div className="bg-white border-2 border-gray-500 rounded-lg shadow pt-6 px-6 pb-2 relative">
+                            <h2 className="text-lg font-semibold text-gray-700 mb-4">講義内容や講義形式がシラバスと異なる</h2>
+                            <p className="text-base text-gray-600">講義内容や講義形式がシラバスに記載してある内容と異なり、履修してみたら自分の興味や目的と異なっていた。</p>
+                            <div className="flex justify-end">
+                                <img src={AboutLogo1} className="w-22 h-20 object-cover rounded-xl" />
+                            </div>
                         </div>
                     </div>
-                    <div class="w-full md:w-1/3 px-4 mb-6">
-                        <div class="bg-white border border-gray-200 rounded-lg shadow p-6">
-                            <h2 class="text-lg font-semibold text-gray-700 mb-4">耳触りのいい企業紹介ばかりで、リアルな情報に出会えない</h2>
-                            <p class="text-sm text-gray-600">実際の昇給率、中途社員の昇給ペース、年齢別の年収、評価制度の実態、休みの取りやすさ、実際の残業時間と有休消化率、入社後のギャップなどのリアルな情報がなかなか見つからない</p>
+                    <div className="w-full md:w-1/3 px-4 mb-6">
+                        <div className="bg-white border-2 border-gray-500 rounded-lg shadow pt-6 px-6 pb-2 relative">
+                            <h2 className="text-lg font-semibold text-gray-700 mb-4">課題や講義時間の負担感の不透明さ</h2>
+                            <p className="text-base text-gray-600">課題の量や頻度、準備にかかる時間について具体的な情報が不足しているため、他の講義や仕事、家事とのバランスが取りにくい。</p>
+                            <div className="flex justify-end">
+                                <img src={AboutLogo2} className="w-22 h-20 object-cover rounded-3xl" />
+                            </div>
                         </div>
                     </div>
 
-                    <div class="w-full md:w-1/3 px-4 mb-6">
-                        <div class="bg-white border border-gray-200 rounded-lg shadow p-6">
-                            <h2 class="text-lg font-semibold text-gray-700 mb-4">多少ネガティブな内容であっても、確認して自分で判断したい</h2>
-                            <p class="text-sm text-gray-600">社内の飲み会が多い、残業時間が長いほど評価される傾向がある、裁量が大きいが自分でやり方を模索する必要がある、自己アピールがうまくないと評価されにくいなど、多少ネガティブな情報も含めて自分に合う会社か判断したい</p>
+                    <div className="w-full md:w-1/3 px-4 mb-6">
+                        <div className="bg-white border-2 border-gray-500 rounded-lg shadow pt-6 px-6 pb-2 relative">
+                            <h2 className="text-lg font-semibold text-gray-700 mb-4">専門性の高さや基礎知識の不足への不安</h2>
+                            <p className="text-base text-gray-600">専門的な内容を扱う講義の場合、自分の基礎知識が十分かどうか分からず、不安を感じる。</p>
+                            <div className="flex justify-end">
+                                <img src={AboutLogo3} className="w-20 h-20 object-cover rounded-3xl" />
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="text-white text-center py-16">
-                    <h1 class="text-center text-4xl font-bold text-gray-700 mb-8">AIIT Cappus Voiceなら!</h1>
-                    <p class="text-lg mt-4 text-gray-700 font-bold">実際に働いている社員や元社員のクチコミ、実際の年収情報がわかるから、後悔しない転職が実現します。</p>
+                <div className="text-white text-center py-10">
+                    <h1 className="text-center text-4xl font-extrabold text-gray-700 mb-8">AIIT Campus Voiceなら!!</h1>
+                    <p className="text-xl mt-4 text-gray-700 font-bold">実際にAIITで学んでいる生徒や元生徒の口コミがわかるから、後悔しないキャンパスライフが実現します。</p>
                 </div>
             </div>
         </>
