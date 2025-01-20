a0cd120e99d67ef47fbde66513351fb5a3915d6a
Daiki <z24075du@aiit.ac.jp>
2025-01-20 19:42:37 +0900

    style: fix code formatting and spacing

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,26 +1,26 @@
-import {Outlet, useLocation} from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar.jsx";
 import Footer from "../components/Footer/Footer.jsx";
-import {useEffect} from "react";
+import { useEffect } from "react";
 
 const Layout = () => {
-    const location = useLocation();
+  const location = useLocation();
 
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, [location]);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
 
-    return (
-        <>
-            <div className="flex flex-col min-h-screen">
-                <Navbar/>
-                <div className="flex-grow">
-                    <Outlet/>
-                </div>
-                <Footer/>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <div className="flex-grow">
+          <Outlet />
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
 };
 
 export default Layout;
