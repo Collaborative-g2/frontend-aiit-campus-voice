a8d220ea8637f3dcda84a59111503dd0c4eb3cbd
Daiki <z24075du@aiit.ac.jp>
2025-01-20 19:41:54 +0900

    feat: reset scroll position to top on page navigation

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,8 +1,15 @@
-import {Outlet} from "react-router-dom";
+import {Outlet, useLocation} from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar.jsx";
 import Footer from "../components/Footer/Footer.jsx";
+import {useEffect} from "react";
 
 const Layout = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [location]);
+
     return (
         <>
             <div className="flex flex-col min-h-screen">
