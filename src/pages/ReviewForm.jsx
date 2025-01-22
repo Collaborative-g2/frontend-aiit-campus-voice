b172e34f0544b56db9858fce8513904d4ef168bc
Daiki <z24075du@aiit.ac.jp>
2025-01-23 00:12:13 +0900

    feat: improve dialog UI for post completion

--- src/pages/ReviewForm.jsx
+++ src/pages/ReviewForm.jsx
@@ -1,11 +1,12 @@
 import { Controller, useForm } from "react-hook-form";
 import { FaCheckCircle, FaTimesCircle } from "react-icons/fa";
 import FormElement from "../components/shared/FormElement.jsx";
-import { MdOutlineRateReview } from "react-icons/md";
+import { MdAddComment, MdOutlineRateReview } from "react-icons/md";
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { ThreeDots } from "react-loader-spinner";
+import { FaHome } from "react-icons/fa";
 
 const ACV_API_BASE_URL = import.meta.env.VITE_ACV_API_BASE_URL;
 
@@ -145,29 +146,51 @@ const ReviewForm = () => {
             {/* Modal */}
             {isModalOpen && (
                 <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-                <div className="bg-white p-6 rounded shadow-lg w-80 flex flex-col items-center">
-                  {statusCode === 200 ? (
-                    <FaCheckCircle className="text-green-500 text-4xl mb-4" />
-                  ) : (
-                    <FaTimesCircle className="text-red-500 text-4xl mb-4" />
-                  )}
-                  <p className="text-gray-700 mb-4">{modalMessage}</p>
-                  <button
-                    onClick={() =>
-                      statusCode === 200
-                        ? navigate("/#latest-reviews")
-                        : setIsModalOpen(false)
-                    }
-                    className={`mt-4 ${
-                      statusCode === 200
-                        ? "bg-green-500 hover:bg-green-700"
-                        : "bg-red-500 hover:bg-red-700"
-                    } text-white font-bold py-2 px-4 rounded`}
-                  >
-                    閉じる
-                  </button>
+                  <div className="bg-white p-6 rounded shadow-lg w-80 flex flex-col items-center">
+                    {statusCode === 200 ? (
+                        <FaCheckCircle className="text-green-500 text-4xl mb-4"/>
+                    ) : (
+                        <FaTimesCircle className="text-red-500 text-4xl mb-4"/>
+                    )}
+                    <p className="text-gray-700 mb-4">{modalMessage}</p>
+                    {statusCode === 200 && (
+                        <p className="text-gray-700 mb-4">ご協力ありがとうございます！</p>
+                    )}
+
+                    <button
+                        onClick={() => setIsModalOpen(false)}
+                        className={`mt-4 ${
+                            statusCode === 200 ? "hidden" : "bg-red-500 hover:bg-red-700"
+                        } text-white font-bold py-2 px-4 rounded`}
+                    >
+                      閉じる
+                    </button>
+
+                    {statusCode === 200 && (
+                        <div className="flex flex-col items-center space-y-4 mt-4">
+                          <button
+                              onClick={() => {
+                                setIsModalOpen(false);
+                                window.location.reload();
+                              }}
+                              className="flex items-center justify-center gap-2 w-48 bg-primary hover:bg-secondary text-white font-bold py-2 px-4 rounded-full"
+                          >
+                            <MdAddComment size={18}/>
+                            続けて投稿する
+                          </button>
+                          <button
+                              onClick={() => {
+                                navigate("/#latest-reviews");
+                              }}
+                              className="flex items-center justify-center gap-2 w-48 bg-primary hover:bg-secondary text-white font-bold py-2 px-4 rounded-full"
+                          >
+                            <FaHome size={18}/>
+                            ホームに戻る
+                          </button>
+                        </div>
+                    )}
+                  </div>
                 </div>
-              </div>
             )}
           </div>
         </div>
